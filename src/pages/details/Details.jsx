44a47a4fcd8b6d395bd1ca2199a1df87ffd0b83a
Gulbeyaz <glbzbayram@gmail.com>
2024-04-20 22:37:55 +0300

    Details page was created

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import "./style.scss";
-
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 const Details = () => {
-  return;
-  <div>Details</div>;
+  const { id } = useParams();
+  const { data, loading, error } = useFetch("i=" + id);
+
+  return (
+    <div>
+      <DetailsBanner id={id} />
+      <Cast data={data?.Actors} loading={loading} />
+      {/* <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} /> */}{" "}
+    </div>
+  );
 };
 
 export default Details;
