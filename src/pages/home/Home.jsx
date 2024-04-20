1a2baad03c2877320558e93addaca0ddadbeaef1
Gulbeyaz <glbzbayram@gmail.com>
2024-04-20 22:35:35 +0300

    Data was defined in homePage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.scss";
+import SearchResult from "./searchResult/SearchResult";
 import HeroBanner from "./heroBanner/HeroBanner";
 
 const Home = () => {
   return (
     <div>
       <HeroBanner />
+      <SearchResult />
     </div>
   );
 };
