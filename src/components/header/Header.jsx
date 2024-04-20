c4d08d114b3516f8145ac38ffec3ed6b83f1e87e
Gulbeyaz <glbzbayram@gmail.com>
2024-04-20 22:33:38 +0300

    Header.jsx was created

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,123 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // window scrolls to top when location changed
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  //it control that header is shown while scrolling
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+        //header is hidden while scrolling to bottom..
+      } else {
+        setShow("show");
+        //header is shown while scrolling to top
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  const searchQueryHandler = (e) => {
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  };
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/series");
+    }
+    setMobileMenu(false);
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo" onClick={() => navigate("/")}>
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("series")}>
+            TV Shows
+          </li>
+          <li className="menuItem">
+            <HiOutlineSearch onClick={openSearch} />
+          </li>
+        </ul>
+
+        <div className="mobile-menu-items">
+          <HiOutlineSearch onClick={openSearch} />
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for a movie or tv show...."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
