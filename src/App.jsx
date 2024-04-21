1c23d8426e08d9e61384107901d451fff5f47403
Gulbeyaz <glbzbayram@gmail.com>
2024-04-21 03:01:57 +0300

    Routes were editted

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Details from "./pages/details/Details";
+import SearchResult from "./pages/home/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:Type/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:type" element={<Explore />} />
       </Routes>
       <Footer />
     </BrowserRouter>
